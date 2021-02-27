34e38ad7fbad8de1606781b44ddebbcf2264ed78
hanoldbuddy <jonathanwise@jonathanwise.com>
2021-02-26 21:07:08 -0600

    Updated HTML Linking

--- Erie/app.py
+++ Erie/app.py
@@ -3,17 +3,37 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
+# wsgi_app = app.wsgi_app
 
 
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about/about.html")
+@app.route("/piano")
+def piano():
+    return render_template("piano/piano.html")
+@app.route("/ham")
+def ham():
+    return render_template("ham/ham.html")
+@app.route("/eng")
+def eng():
+    return render_template("eng/eng.html")
+@app.route("/drone")
+def drone():
+    return render_template("drone/drone.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact/contact.html")
+@app.route("/ai")
+def ai():
+    return render_template("ai/ai.html")
 
 if __name__ == '__main__':
     import os
